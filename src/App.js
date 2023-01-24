bb5f32653c1b2976bee0ec47bc760f11a6f4184d
Jgithubacob123! <jakenaumann@yahoo.com>
2023-01-24 14:45:18 -0500

    First commit App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [timings, setTimings] = useState([]);
+  const [startTime, setStartTime] = useState();
+  const [letters, setLetters] = useState([]);
+  const [length, setLength] = useState([]);
+
+  function handleInputChange(event) {
+    const currentLetter = event.target.value.slice(-1);
+    setLetters((prevLetters) => [...prevLetters, currentLetter]);
+    if (!startTime) {
+      setStartTime(Date.now());
+    } else {
+      const currentTime = Date.now();
+      const timing = currentTime - startTime;
+      setTimings((prevTimings) => [...prevTimings, timing]);
+      setStartTime(currentTime);
+    }
+    setInputValue(event.target.value);
+    setLength(event.target.value.length);  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={inputValue} onChange={handleInputChange} />
+         {timings.map((timing, timingIndex) => (
+          <div key={timingIndex}>key was {timing}ms behind.(input is {length} long)</div>
+        ))}    
+    </>
   );
 }
 
